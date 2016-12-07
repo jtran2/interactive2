f70f75ed7b086c3ece5915391ffdf59260f8ec8e
jtran2 <jtran2@cca.edu>
2016-12-07 04:08:05 -0800

    edits

---
+++ project3/js/myscript.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+$(function() {
+    $('.ornament').on('mousedown', 'div', function() {
+        $(this).addClass('draggable').parents().on('mousemove', function(e) {
+            $('.draggable').offset({
+                top: e.pageY - $('.draggable').outerHeight() / 2,
+                left: e.pageX - $('.draggable').outerWidth() / 2
+            }).on('mouseup', function() {
+                $(this).removeClass('draggable');
+            });
+        });
+        e.preventDefault();
+    }).on('mouseup', function() {
+        $('.draggable').removeClass('draggable');
+    });
+});
+});
